96cfa880857899e42e8cdfc4a6505fd7435d2b38
Carson-NNY <gl2852@columbia.edu>
2025-07-04 13:04:51 -0400

    s

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -14,7 +14,7 @@ interface ButtonProps {
   blurBackground?: boolean;
 }
 
-// customizable button component, displayed based on the pattern prop. make)
+// customizable button component, displayed based on the pattern prop.)
 export default function Button({
   pattern,
   onClick,
